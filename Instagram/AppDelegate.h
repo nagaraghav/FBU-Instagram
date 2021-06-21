ce2a44f8593b40d3947244515e5d21aa8b6590ad
nagaraghav <snraghav1122@gmail.com>
2021-06-20 17:15:37 -0700

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Raghav Sreeram on 6/20/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
